2b1680eebf5ac7bc8b6b500546fdc00cada01fe8
Yana <yanama758@gmail.com>
2025-10-15 17:11:53 +0300

    add smth

--- Project12_ierarhia/Source.cpp
+++ Project12_ierarhia/Source.cpp
@@ -30,7 +30,31 @@ public:
         : goods(name, number, price) {
         this->voltage = voltage;
     }
-};//laptop/Pc - >game
+};
+
+class pc : public electro {
+    bool game; // 1 - pc for game; 0 - basic pc
+public:
+    pc() : electro() {
+        game = false;
+    }
+    pc(string name, int number, int price, int voltage, bool game)
+        : electro(name, number, price, voltage) {
+        this->game = game;
+    }
+};
+
+class printer : public electro {
+    bool color_or_not; // 1 - Rgb; 0 - no rgb
+public:
+    printer() : electro() {
+        color_or_not = false;
+    }
+    printer(string name, int number, int price, int voltage, bool color_or_not)
+        : electro(name, number, price, voltage) {
+        this->color_or_not = color_or_not;
+    }
+};
 
 class food : public goods {
     int calories;
